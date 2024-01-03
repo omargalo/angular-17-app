3f5842872aa3d41b59a935a67bd082fa0e52e2a5
Omar García López <omar.garcia@omargl.net>
2024-01-02 19:32:04 -0600

    app user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet, UserComponent],
+  templateUrl: './app.component.html',  
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-17-app';
+  city = 'CIUDAD DE MÉXICO';
 }
